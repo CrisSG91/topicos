bb5ca4133a4ffa606d283dccea83d3b3cdce06ff
Tey García Miranda <teygarciam@hotmail.com>
2017-09-12 18:52:21 -0600

    Commit de prueba (hola mundo)

--- Ulatina.Electiva.Calentamiento/GatoGenerico/Program.cs
+++ Ulatina.Electiva.Calentamiento/GatoGenerico/Program.cs
@@ -10,6 +10,8 @@ namespace GatoGenerico
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
